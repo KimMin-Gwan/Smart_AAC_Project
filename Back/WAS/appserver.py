9a8fe186ad9b3e743e33e9f9bbf3c6dce04d95b1
김민관 <alsrhks2508@naver.com>
2023-07-01 17:28:57 +0900

    Update appserver.py

--- Back/WAS/appserver.py
+++ Back/WAS/appserver.py
@@ -1,20 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-#from pydantic import BaseModel
+from pydantic import BaseModel
 from fastapi import UploadFile, File
 import soundfile as sf
-#import Mainfucntion as mainfunction
-
-def get_audio_header_info(filename):
-    # 오디오 파일의 헤더 정보를 읽어옴
-    info = sf.info(filename)
-
-    # 헤더 정보 출력
-    print("Sample Rate:", info.samplerate)
-    print("Channels:", info.channels)
-    print("Duration:", info.duration)
-    print("Format:", info.format)
-    print("Subtype:", info.subtype)
+import Mainfucntion as mainfunction
 
 app = FastAPI()
 
@@ -22,14 +11,22 @@ app = FastAPI()
 def main():
     return 'The Compass Server'
 
-#class Model(BaseModel):
-    #name : str
-
 #wav 데이터 받기
 @app.post('/get_wav')
 async def recog_voice(file: UploadFile = File(..., required = False)):
-    contents = await file.read()
-    return {"filename": 'hi'}
+    if file is None:
+        print('wav file did not uploaded')
+        return {"error": "No file provided"}
+    else:
+        print('wav file upload complete')
+
+    result = mainfunction.recog_wav()
+
+    if result == 'default':
+        return {"error" : "Recognize Fail"}
+
+    return {"result" : result}
+
 
 #GPS 데이터 보내기
 @app.post('/get/id')
@@ -41,10 +38,8 @@ async def recog_GPS():
 """
 1. 메인 ( /)
 2. 음성 데이터 보내기( /post/wav)
-3. 음성 분석 데이터 받기(/get/wav)
-4. GPS 데이터 보내기( /post/gps)
-5. GPS 데이터 받기(/get/gps)
-6. 사용자 정보 보내기( /post/id)
-7. 사용자 정보 받기 (/get/id)
+3. GPS 데이터 보내기( /post/gps)
+4. 사용자 정보 보내기( /post/id)
+5. 사용자 정보 받기 (/get/id)
 """
 
