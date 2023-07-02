c58442bfa98e1537d19cc95d60acd7fe871ae78f
김민관 <alsrhks2508@naver.com>
2023-07-02 18:32:58 +0900

    테스팅용 마스터 파일

--- Back/python/master.py
+++ Back/python/master.py
@@ -1,4 +1,11 @@
-from main_process import MainFunction
+from PROCESS import MainFunction
+from WAS import AppServer
+import uvicorn 
 
 def server():
     mainfunction = MainFunction()
+    server = AppServer(mainfunction=mainfunction)
+    app = server.app
+    uvicorn.run(app, host = '127.0.0.1', port = 7777)
+
+
