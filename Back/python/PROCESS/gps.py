9a79269d4fb24576bf79d9f9e5c42271ce6f19bd
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-02 21:22:26 +0900

    gps class 이동

--- Back/python/PROCESS/gps.py
+++ Back/python/PROCESS/gps.py
@@ -1,17 +1,35 @@
 import requests
 from urllib.parse import urlparse
 import json
+import constant
 
-header={"Authorization": "KakaoAK 468b7fd043830d2a5ddb6fff0a000b5b"}  #<<이거 밖으로 뺴는게 좋아보임
+class gps():   
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 
-#좌표로 주소 변환하기
-def gps_data(lon, lat):
-    url = f"https://dapi.kakao.com/v2/local/geo/coord2address.json?x={lon}&y={lat}"
-    result = requests.get(urlparse(url).geturl(),
-        headers=header).json()
+    def __gps_location(self):
+        #안드로이드 애플리케이션에서 gps 데이틀 받아옴
+        return 0
 
-    return result
+    def __where_am_i(self):
+        #gps 경도 위도로 현재 위치의 정보 받기
+        lon = self.__gps_location() #gps 위치 받은거 경도 위도 잘라서 넣어주기
+        lat = 0
+        url = f"https://dapi.kakao.com/v2/local/geo/coord2address.json?x={lon}&y={lat}"
+        result = requests.get(urlparse(url).geturl(), headers=constant.header).json()        
+        return result
 
-#확인용 출력
-print(json.dumps(gps_data(128.755739,35.834908),indent=4,ensure_ascii=False))  
-print(json.dumps(gps_data(128.755739,35.834908)['documents'][0]["road_address"]["building_name"],indent=4,ensure_ascii=False))
+    def __you_are_here(self):
+        try:
+            data = json.dumps(self.__where_am_i()['documents'][0]["road_address"]["building_name"],indent=4,ensure_ascii=False)
+            return 
+        
+        except:
+            all_data = print("건물 데이터 못찾음 대신 다 보여드림", json.dumps(self.__where_am_i(),indent=4,ensure_ascii=False))
+            return 
+
+
+    def need_great_name_4_this_func(self):
+        #건물 이름으로 얻은 데이터 분석해서 장소 라벨 보내주기 ex)식당, 병원, 약국, 학교
+        return 0
