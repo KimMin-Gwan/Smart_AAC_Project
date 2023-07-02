212d487259d86c48540d9f96a6afcee545174b51
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-02 19:12:56 +0900

    이름변경

--- Back/python/PROCESS/gps.py
+++ Back/python/PROCESS/gps.py
@@ -5,13 +5,13 @@ import json
 header={"Authorization": "KakaoAK 468b7fd043830d2a5ddb6fff0a000b5b"}  #<<이거 밖으로 뺴는게 좋아보임
 
 #좌표로 주소 변환하기
-def notNamed2(x, y):
-    url = f"https://dapi.kakao.com/v2/local/geo/coord2address.json?x={x}&y={y}"
+def gps_data(lon, lat):
+    url = f"https://dapi.kakao.com/v2/local/geo/coord2address.json?x={lon}&y={lat}"
     result = requests.get(urlparse(url).geturl(),
         headers=header).json()
 
    #확인용 출력
-    print(json.dumps(notNamed2(128.755739,35.834908),indent=4,ensure_ascii=False))  
+    print(json.dumps(gps_data(128.755739,35.834908),indent=4,ensure_ascii=False))  
        
     return result
 
