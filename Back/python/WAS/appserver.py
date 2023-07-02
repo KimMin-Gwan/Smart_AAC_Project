1a913d339cab9f11551aad71c5d2f8007a288281
김민관 <alsrhks2508@naver.com>
2023-07-02 18:27:20 +0900

    서버 패키지

---
+++ Back/python/WAS/appserver.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi import UploadFile, File
+import soundfile as sf
+import Mainfucntion as mainfunction
+
+app = FastAPI()
+
+@app.get('/')
+def main():
+    return 'The Compass Server'
+
+#wav 데이터 받기
+@app.post('/get_wav')
+async def recog_voice(file: UploadFile = File(..., required = False)):
+    if file is None:
+        print('wav file did not uploaded')
+        return {"error": "No file provided"}
+    else:
+        print('wav file upload complete')
+
+    result = mainfunction.recog_wav()
+
+    if result == 'default':
+        return {"error" : "Recognize Fail"}
+
+    return {"result" : result}
+
+# GPS 데이터 형식
+class GPS(BaseModel):
+    x:str
+    y:str
+
+#GPS 데이터 보내기
+@app.post('/get_gps')
+async def recog_GPS(data : GPS):
+    if data is None:
+        print('gps not uploaded')
+        return {"error": "data did not usable"}
+
+    x = int(data.x)
+    y = int(data.y)
+    # result:str
+    result = mainfunction.recog_gps(x, y)
+
+    return {"Category": result}
+
+
+"""
+1. 메인 ( /)
+2. 음성 데이터 보내기( /post/wav)
+3. GPS 데이터 보내기( /post/gps)
+4. 사용자 정보 보내기( /post/id)
+5. 사용자 정보 받기 (/get/id)
+"""
+
---
+++ Back/python/WAS/constant.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+# GPS 데이터 형식
+class GPS(BaseModel):
+    x:str
+    y:str
