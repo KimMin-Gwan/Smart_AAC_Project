5550d0b97643f0804ca5178a976c76f6163aa9cd
김민관 <alsrhks2508@naver.com>
2023-07-02 19:25:18 +0900

    마스터파일 수정

--- Back/python/master.py
+++ Back/python/master.py
@@ -1,11 +1,13 @@
 from PROCESS import MainFunction
 from WAS import AppServer
 import uvicorn 
+HOST = '127.0.0.1'
+PORT = 7777
 
 def server():
     mainfunction = MainFunction()
     server = AppServer(mainfunction=mainfunction)
-    app = server.app
-    uvicorn.run(app, host = '127.0.0.1', port = 7777)
+    server.run_server(HOST = HOST, PORT = PORT)
+    
 
 
