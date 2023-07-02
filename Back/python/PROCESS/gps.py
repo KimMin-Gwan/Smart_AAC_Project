527fef1966f4d6e104b2e9820ce6d89d2aa8ea4c
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-02 21:27:18 +0900

    이름 변경

--- Back/python/PROCESS/gps.py
+++ Back/python/PROCESS/gps.py
@@ -12,7 +12,7 @@ class gps():
         #안드로이드 애플리케이션에서 gps 데이틀 받아옴
         return 0
 
-    def __where_am_i(self):
+    def __get_location(self):
         #gps 경도 위도로 현재 위치의 정보 받기
         lon = self.__gps_location() #gps 위치 받은거 경도 위도 잘라서 넣어주기
         lat = 0
@@ -20,7 +20,7 @@ class gps():
         result = requests.get(urlparse(url).geturl(), headers=constant.header).json()        
         return result
 
-    def __you_are_here(self):
+    def __location_name(self):
         try:
             data = json.dumps(self.__where_am_i()['documents'][0]["road_address"]["building_name"],indent=4,ensure_ascii=False)
             return 
@@ -30,6 +30,6 @@ class gps():
             return 
 
 
-    def need_great_name_4_this_func(self):
+    def gps_analyzer(self):
         #건물 이름으로 얻은 데이터 분석해서 장소 라벨 보내주기 ex)식당, 병원, 약국, 학교
         return 0
