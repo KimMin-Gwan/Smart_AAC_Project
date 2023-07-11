687bef3e26e9fceaa7f7f8bd6c2ad69abb0e05b2
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-11 17:30:49 +0900

    네이버 api 테스트

--- Back/test_Vespoi/naver_api_.py
+++ Back/test_Vespoi/naver_api_.py
@@ -2,8 +2,8 @@ import requests
 from urllib.parse import urlparse
 import json
 
-client_id = '---------------------' 
-client_secret = '-----------------------'
+client_id = '_________________________' 
+client_secret = '__________________________'
 headers = {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
 
 url_base="https://openapi.naver.com/v1/search/local.json?query="
@@ -15,3 +15,4 @@ url = url_base + keyword + url_middle + number
 
 result = requests.get(url,headers = headers).json()
 print(json.dumps(result,indent=4,ensure_ascii=False))
+print(json.dumps(result['items'][0]["category"],indent=4,ensure_ascii=False))
