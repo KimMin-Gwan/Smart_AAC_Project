57322dde7ffac4a7b01a27f242a88d31ff6776e4
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-11 17:28:57 +0900

    네이버 api 테스트

---
+++ Back/test_Vespoi/naver_api_.py
@@ -0,0 +1,17 @@
+import requests
+from urllib.parse import urlparse
+import json
+
+client_id = '---------------------' 
+client_secret = '-----------------------'
+headers = {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret':client_secret}
+
+url_base="https://openapi.naver.com/v1/search/local.json?query="
+keyword = "영남대학교"
+url_middle="$&start="
+number='1'
+
+url = url_base + keyword + url_middle + number
+
+result = requests.get(url,headers = headers).json()
+print(json.dumps(result,indent=4,ensure_ascii=False))
