f2e4851b1b305d812290201b7f7abf7810c44830
김민관 <alsrhks2508@naver.com>
2023-07-17 13:20:22 +0900

    Update check_dataset.py

--- Back/text_model/check_dataset.py
+++ Back/text_model/check_dataset.py
@@ -1,10 +1,10 @@
 import json
 
 #FILE_PATH = './../../../text/한국어_대화/대화데이터/'
-#FILE_NAME = 'korean_dataset.json'
+FILE_NAME = 'korean_intence.json'
 
 FILE_PATH = './'
-FILE_NAME = 'new_korean_dataset.json'
+FILE_NAME = 'new_korean_intence.json'
 
 with open(FILE_PATH + FILE_NAME, 'r', encoding='UTF8') as f:
     json_data = json.load(f)
@@ -15,7 +15,7 @@ intence = json_data['intence']
 for data in intence:
     count_patterns = len(data['patterns'])
     patterns_len_list.append(count_patterns)
-    if count_patterns >= 4:
+    if count_patterns >= 10:
         tag = data['tag']
         tag_list.append(tag)
     
@@ -37,7 +37,7 @@ new_json_data = {'intence' : intence}
 
 #print(json.dumps(json_data, indent="\t"))
 
-with open( './new_korean_dataset.json', 'w', encoding='utf-8') as f : 
+with open( './new_korean_intence.json', 'w', encoding='utf-8') as f : 
     json.dump(json_data, f, indent=4, ensure_ascii=False)
 
         
