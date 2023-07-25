9a6627f54a392178fe752261f757697d29d55730
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-07-25 18:48:48 +0900

    json 만들 때 쓸 꺼 수정

--- Back/test_Vespoi/jsonEditor.py
+++ Back/test_Vespoi/jsonEditor.py
@@ -14,12 +14,12 @@ def id_finder_multi(dict_data, name_list):
                 id_list.append(i["id"])
     return id_list
 
-def node_adder_single(dict_data, id, node):
+def node_add_single(dict_data, id, node):
     for i in dict_data["AAC"]:
         if(i["id"]==id):
             i["node"].append(node)
 
-def node_adder_multi(dict_data, id, node):
+def node_add_multi(dict_data, id, node):
     for i in dict_data["AAC"]:
         if(i["id"]==id):
             for j in node:
@@ -27,27 +27,46 @@ def node_adder_multi(dict_data, id, node):
 
 def save_json(dict_data):
     json_data = json.dumps(dict_data, indent="\t", ensure_ascii=False)
-    with open('json_data.json', 'w') as f:
+    with open('json_data_edit.json', 'w') as f:
         f.write(json_data)
 
+def add_multi_to_multi(dict_data, list_where):
+    for i in list_where:
+        id = id_finder(dict_data, i)
+        node_add_multi(dict_data, id, node_list)
+
+def add_single_to_multi(dict_data, list_where, name_single):
+    for i in list_where:
+        id = id_finder(dict_data, i)
+        node_add_single(dict_data, id, name_single)
 
 
-with open ("json_data.json", "r") as f:
+with open ("json_data_edit.json", "r") as f:
     dict_data = json.load(f)
 
-name_single = id_finder(dict_data,"재건축아파트정보세대수층연식위치등") #추가할거 하나 이름
 
-name_list = ["임대기간", "재건축아파트정보세대수층연식위치등"] #추가할거 여러개 이름
-node_list = id_finder_multi(dict_data, name_list)
+id = id_finder(dict_data,"") #어디에 추가할지 이름
+list_where = ["", "", ""] #여러개에 한번에 추가 할 때 추가하는 곳 이름
 
-id = id_finder(dict_data,"재건축아파트정보세대수층연식위치등") #어디에 추가할지 이름
+
+name_single = id_finder(dict_data,"") #추가할거 하나 이름
+name_list = ["", "", ""] #추가할거 여러개 이름
+
 
 #print(id_finder(dict_data,"재건축아파트정보세대수층연식위치등"))
 #print(id_finder_multi(dict_data, name_list))
 
-node_adder_single(dict_data, id, name_single) #1개 추가
-node_adder_multi(dict_data, id, node_list) #여러개 추가
+node_add_single(dict_data, id, name_single) #1개 추가
+
+
+node_list = id_finder_multi(dict_data, name_list)
+node_add_multi(dict_data, id, node_list) #여러개 추가
+
+
+add_single_to_multi(dict_data, list_where, name_single) #여러개에 하나 추가
+add_multi_to_multi(dict_data, list_where) #여러개에 여러개 추가
+
 
 save_json(dict_data)
 
-#만들어야 하는거: 삭제
+#만들어야 하는거: 삭제 , 수정(초기화)
--- Back/test_Vespoi/makeJson.py
+++ Back/test_Vespoi/makeJson.py
@@ -3,7 +3,7 @@ import json
 
 
 def preprocessing_text_data(file_path):
-    file = open(ai_file_path, encoding='UTF8')
+    file = open(file_path, encoding='UTF8')
 
     data = file.read()
 
@@ -11,7 +11,7 @@ def preprocessing_text_data(file_path):
 
     text_data = []
     for i in split_data:
-        new_str = re.sub(r"[^\uAC00-\uD7A3a-zA-Z\s]", "", i)
+        new_str = re.sub(r"[^\uAC00-\uD7A3a-zA-Z\s0-9]", "", i)
         new_str = new_str.replace(" ", "")
         #print(new_str)
         text_data.append(new_str)
@@ -30,7 +30,7 @@ def make_dict_location(dict_aac,aac_data):
         })
 
 def make_dict_AAC(dict_aac,aac_data):
-    aac_label = "100" #기본 AAC
+    aac_label = "20" #기본 AAC
     index = 0
     for i in aac_data:
         index = index + 1
@@ -41,7 +41,7 @@ def make_dict_AAC(dict_aac,aac_data):
         })
 
 def make_dict_ai(dict_aac,aac_data):
-    ai_label = "200" #AI 카테고리
+    ai_label = "90" #AI 카테고리
     index = 0
     for i in aac_data:  
         index = index + 1
@@ -62,11 +62,14 @@ def make_json(dict_data):
 # print(type(json_data))
 #print(dict_data)
 
+location_file_path = '카테고리/test_location.txt'
+AAC_file_path = '카테고리/test_AAC.txt'
 ai_file_path = '카테고리/category.txt'
 
+location = preprocessing_text_data(location_file_path)
+AAC = preprocessing_text_data(AAC_file_path)
 ai_data = preprocessing_text_data(ai_file_path)
-location = ["카페","병원","식당","학교"]
-AAC = ["커피","에이드"]
+
 dict_aac = {"AAC": [] }
 
 make_dict_location(dict_aac,location)
