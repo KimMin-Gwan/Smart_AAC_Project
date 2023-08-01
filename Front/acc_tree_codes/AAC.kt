836681e44410f5b1cb3ddf143c1f7b692b59bb6d
김민관 <alsrhks2508@naver.com>
2023-08-01 13:21:37 +0900

    Data Class

---
+++ Front/acc_tree_codes/AAC.kt
@@ -0,0 +1,5 @@
+package com.example.new_test.ui.theme
+
+data class AAC(
+    val AAC: List<AACX>
+)
---
+++ Front/acc_tree_codes/AACX.kt
@@ -0,0 +1,7 @@
+package com.example.new_test.ui.theme
+
+data class AACX(
+    val id: Int,
+    val name: String,
+    val node: List<Int>
+)
