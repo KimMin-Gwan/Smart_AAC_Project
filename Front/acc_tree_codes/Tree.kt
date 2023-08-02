152a662c6c712bd00480952ba3ab39def4a1dd3f
김민관 <alsrhks2508@naver.com>
2023-08-02 14:44:15 +0900

    Update Tree.kt

--- Front/acc_tree_codes/Tree.kt
+++ Front/acc_tree_codes/Tree.kt
@@ -15,79 +15,6 @@ enum class NodeStatus {
     Disable
 }
 
-// node
-class Node(){
-    var _id : Int = -1
-    var name : String = "name"
-    var node_status : NodeStatus = NodeStatus.NOT_USING
-    var child_index_list = ArrayList<Int>()
-    // --------------------------------------------------
-    fun setID(id : Int){this._id = id}
-    fun getID() : Int  = this._id
-    @JvmName("callFromString")
-    fun setName(name : String) {this.name = name}
-    @JvmName("callFromString")
-    fun getName() : String = this.name
-    fun setNodeStatus( status : NodeStatus) { this.node_status = status}
-    fun getNodeStatus() : NodeStatus = this.node_status
-    // 자식 노드 인덱스 세팅 (만약 자식이 없다면 0번째 데이터가 -1)
-    fun setChildIndexList(index_list : List<Int>) {
-        for (arg in index_list)
-            child_index_list.add(arg)
-    }
-    fun getChildIndexList() : ArrayList<Int> = this.child_index_list
-    // 노드 생성자 (인덱스, 이름, 자식 노드)
-    fun node_init(id : Int, name : String, index_list : ArrayList<Int>) {
-        this._id = id; this.name = name; this.child_index_list = index_list
-    }
-    // == 오버라이딩
-    override fun equals(other : Any?) : Boolean {
-        if ( this === other ) return true
-        if ( other !is Node ) return false
-        return this._id == other._id && this.name == other.name
-    }
-    // print 오버라이딩
-    override fun toString() : String{
-        return "Vertex id : $this._id, Vertex name : $this.name, Status : ${this.node_status}"
-    }
-}
-
-// 노드를 가지고 있는 리스트
-class NodeList(var size : Int){
-    var node_Array = Array(size) { Node() }
-    // 루트 노드를 미리 저장
-    //var root_node : Array<Int> = emptyArray()
-    var num_node : Int = 0
-    init{
-        println("Create Node  List | ( $this.num_nodes ) size) ")
-    }
-
-    // 노드 접근자
-    fun getNode(index : Int) : Node {
-        if (this.isInvalid(index))
-            return this.node_Array[index]
-        else
-            throw ArithmeticException("Invalid Execss !")
-            //return Node()
-    }
-    @JvmName("callFromInt")
-    fun getSize() : Int = this.num_node
-    // 노드 인덱스에 맞춰서 배열에 삽입
-    fun addNode(node : Node){
-        if (this.num_node < this.size){
-            val index = node.getID()
-            node_Array[index] = node
-            this.num_node++
-        }
-        else
-            println("NodeList is FULL !!!")
-    }
-    // valid 한 index인지 검사
-    fun isInvalid(index : Int) : Boolean{
-        if (index >= 0 && index < this.size) return true else return false
-    }
-}
-
 // 트리 구조에서 생성될 실제 노드와 포인터
 /// 자신 : self_node, 자식 : child_node
 class Tree_Node(val self_node : Node) {
@@ -118,6 +45,7 @@ class AAC_Tree(var root_index :Int, val node_list : NodeList)
     // nodes[0] == root
     val nodes = ArrayList<Tree_Node>()
     var root : Node = Node()
+    var child_history = ArrayList<ArrayList<Int>>() // 자식 배열 정리
     init{
         try{
             this.root = node_list.getNode(this.root_index)
@@ -164,55 +92,125 @@ class AAC_Tree(var root_index :Int, val node_list : NodeList)
     }
 
     //var latest_pick : Tree_Node = this.nodes[0]
-    var num_pick : Int = 0
     var pick_history = ArrayList<Tree_Node>()
+    var now_layer : Int = 0
+    var pick_string : ArrayList<String> = arrayListOf()
 
     // ***** 트리 생성후 바로 루트 노드 받아갈 것  ********
     fun getRootNode() : Tree_Node {
-        this.num_pick ++
-        pick_history.add(this.nodes[0])
+        val childHistory1d = ArrayList<Int>()
+        childHistory1d.add(this.root_index)
+        this.child_history.add(childHistory1d)
+        this.now_layer += 0
         return this.nodes[0]
     }
-    // 자식 노드가 있는지 확인해준다. 없다면 false 반환
-    fun isChildExist( now_node : Tree_Node ) : Boolean {
-        return now_node.getNumChild() > 0
+
+    // 자식 노드가 있는지 확인해준다. 없다면 -2반환, 있다면  -1 반환
+    // 선탞된 노드 위치가 이상 layer 값을 반환
+    /*
+    1. 현재 레이어 위치에 있는 노드가 맞는지 확인한다.
+    2. 현재 레이어 위치에 있다면 자식노드가 있는지 확인한다.
+    3. 자식노드가 있다면 -1을 반환
+    4. 자식노드가 없다면 -2를 반환
+    5. 현재 레이어 위치에 없다면 몇번째 레이어인지 확인
+    6. 찾아낸 레이어 위치를 반환
+    */
+    fun isChildExist( now_node : Tree_Node ) : Int{
+        // 1. 현재 레이어 위치에 있는 노드가 맞는지 확인한다.
+        //println(child_history[this.now_layer])
+        if (child_history[this.now_layer].contains(now_node.getId())) {
+            // 2. 현재 레이어 위치에 있다면 자식노드가 있는지 확인한다.
+            if (now_node.getNumChild() > 0)
+                // 3. 자식노드가 있다면 -1을 반환
+                return -1
+            else
+                // 4. 자식노드가 없다면 -2를 반환
+                return -2
+        }
+        else {
+            for (idx in 0..child_history.size) {
+                // 5. 현재 레이어 위치에 없다면 몇번째 레이어인지 확인
+                if (child_history[idx].contains( now_node.getId()))
+                    // 6. 찾아낸 레이어 위치를 반환
+                    return idx
+                else
+                    continue
+            }
+            throw ArithmeticException("Could not find now_node")
+        }
     }
     // 아래 함수를 실행하기 전에 isChildExist()로 자식을 확인할것
-    fun selectNodeFlow(now_node : Tree_Node) : ArrayList<Tree_Node> {
+    // 만약
+    fun selectNodeFlow(now_node : Tree_Node,  layer : Int = -1) : ArrayList<Tree_Node> {
         // 자식 수가 0이면 null을 반환
         now_node.setNodeStatus(NodeStatus.USING)
         //if (now_node.getNumChild() <= 0) {
             //return null}
-        // 후진인지 확인, 후진이라면 히스토리에서 마지막 제거
-        // 정상동작 하지않음
-        /*
-        if (pick_history.contains(now_node)){
-            var remove_node : Tree_Node = this.pick_history.removeLast()
-            remove_node.setNodeStatus(NodeStatus.NOT_USING)
-        }
-        else
-        */
-            this.pick_history.add(now_node)
+        // layer 값이 양수라면 후진
+        if (layer >= 0)
+            this.__nodeFlowBackword(now_node, layer)
 
+        this.pick_history.add(now_node)
+        this.now_layer += 1
+        this.__setPickString() // 스트링 생성
+
+        /*
+        1. 자식 노드 받아옴
+        2. 자식 노드 어레이 리스트에 저장
+        3. 2에서 저장한 배열을 한번 더 저장
+        */
+        val childHistory1d = ArrayList<Int>()
+
+        // 자식 노드 받아오기
         var child_nodes : ArrayList<Tree_Node> = now_node.getChild()
+        for (arg in child_nodes)
+        {
+            childHistory1d.add(arg.getId())
+        }
+        this.child_history.add(childHistory1d)
         return child_nodes
     }
-
-    // 사용 내역을 지우고, 노드 스테이터스를 제거
-    fun clearTree(){
-        for (i in 0..num_pick)
-        {
-            var remove_node : Tree_Node = this.pick_history.removeLast()
-            remove_node.setNodeStatus(NodeStatus.NOT_USING)
+    // 이전에 존재하던 선택되지 않은 노드가 다시 선택됨
+    private fun __nodeFlowBackword(now_node : Tree_Node, layer : Int){
+        val temp_child_history : ArrayList<Int> = this.child_history[layer]
+        if (layer != -1){
+            // 현재 선택된 노드의 레이어 층 아래를 모두 지운다
+            for (idx in (layer + 1)..(this.child_history.size - 1)) {
+                this.child_history.removeAt(layer + 1)
+            }
+            // 노드 사용기록들 중 현재 선택한 노드 뒤를 모두 지움
+            for (idx in (layer) .. (this.pick_history.size - 1)) {
+                this.pick_history[layer].setNodeStatus((NodeStatus.NOT_USING))
+                this.pick_history.removeAt(layer)
+            }
+            this.__setPickString(false) // 스트링 생성
+            this.now_layer = layer
+        }
+        else
+            throw ArithmeticException("Invalid Index")
+        return
+    }
+    // 선택된 스트링 생성
+    private fun __setPickString(flag : Boolean = true){
+        // flag == true 이면 뒤에 추가
+        if (flag)
+            this.pick_string.add(this.pick_history[this.now_layer - 1].getName())
+        // flag == false 이면 다지우고 다시 추가
+        else {
+            this.pick_string.clear()
+            for (arg in this.pick_history)
+                this.pick_string.add(arg.getName())
         }
     }
+    // 스트링 접근자
+    fun getString() : ArrayList<String> = this.pick_string
 }
 
 
 class MakeNode()
 {
     val gson = Gson()
-    val JSON_PATH = "D:\\kotlin\\app\\src\\main\\assets\\json_data_edit.json"
+    val JSON_PATH = "D:\\kotlin\\app\\src\\main\\assets\\json_data_test.json"
     val fileContent = File(JSON_PATH).readText()
     val datas : AAC = gson.fromJson(fileContent, AAC::class.java)
     val size : Int = datas.AAC.last().id
@@ -231,20 +229,72 @@ class MakeNode()
     }
 }
 
+
+// Root_Category maker
+class Make_Root_Category()
+{
+    val min_index = 101
+    val max_index = 200
+    // 루트 카테고리는 Integer형태로, 인덱스만 보관
+    var root_category = ArrayList<Int>()
+    var size : Int = 0
+
+    // 기본적인 루트 카테고리 세팅
+    fun get_default_root_category(node_list : NodeList) : ArrayList<Int>
+    {
+        // 100부터 200사이에 루트노드를 검색
+        for (idx in this.min_index..this.max_index)
+        {
+            if (node_list.isInvalid(idx))
+            {
+                this.root_category.add(idx)
+                this.size += 1
+            }
+            // 100 부터 순차적으로 찾아볼때 invalid함 데이터면 더이상 루트노드는 없다
+            else
+                break
+        }
+        return this.root_category
+    }
+
+    // gps를 통해 카테고리를 추천할 때
+    fun get_gps_root_category(node_list : NodeList, gps_data : ArrayList<Int>) :ArrayList<Int>
+    {
+        for ( arg in gps_data )
+        {
+            if (this.root_category.isEmpty() || !this.root_category.contains(arg))
+                throw ArithmeticException("Invalid Index")
+            // 데이터 삭제
+            this.root_category.remove(arg)
+
+            // 맨 앞으로 옮기기
+            this.root_category.add(0, arg)
+        }
+        return this.root_category
+    }
+
+    fun get_root_category_size() : Int = this.size
+}
+
+
 fun main(){
     // 노드 생성 ( 앱 실행 초기 상태)
-    var make : MakeNode = MakeNode()
-    val node_list : NodeList = make.make_node_list()
-
+    val node_maker : MakeNode = MakeNode()
+    val root_maker : Make_Root_Category = Make_Root_Category()
+    val node_list : NodeList = node_maker.make_node_list()
+    val root_category : ArrayList<Int> = root_maker.get_default_root_category(node_list)
+    print("root_category : ")
+    println(root_category)
     // 루트 노드 지정 ( 여기선 101 == 카페 로 지정)
-    val node : Node = node_list.getNode(101)
-
+    val node : Node = node_list.getNode(root_category[0])
     // aac tree 생성
     val aac_tree = AAC_Tree(101, node_list)
     // tree에서 루트 노드 접근
     var now_node : Tree_Node = aac_tree.getRootNode()
+    var temp_node : Tree_Node = aac_tree.nodes[2]
     // 자식 노드 리스트 초기화
     var child_node_list : ArrayList<Tree_Node> = arrayListOf()
+
 
     var pick_index : Int = 0
 
@@ -259,15 +309,15 @@ fun main(){
         println(" ")
 
         // 자식이 존재하는지 확인
-        if (aac_tree.isChildExist(now_node)) {
+        var flag = aac_tree.isChildExist((now_node))
+        print("flag : ")
+        println(flag)
+        if (flag >= -1) {
             // 자식이 있다면 선택된 노드의 자식 리스트 접근
-            child_node_list = aac_tree.selectNodeFlow(now_node)
+            child_node_list = aac_tree.selectNodeFlow(now_node, flag)
             // 사이즈 체크
-            print("now_node size : ")
-            print(child_node_list.size)
-            println(" ")
-            print("childe : ")
             // 반환된 노드 리스트가 있는지 확인
+            print("child : ")
             if (child_node_list != null) {
                 for (child in child_node_list)
                 {
@@ -275,22 +325,31 @@ fun main(){
                     print(", ")
                 }
             }
+
+
             // 선택한 노드 (임의)
+            // 4021번일때 1번 레이어의 2010번 노드로 재진입 한다고 가정
             if (pick_index < child_node_list.size) {
-                now_node = child_node_list[pick_index]
-                pick_string.add(child_node_list[pick_index].getName())
+                if (now_node.getId() == 101){
+                    temp_node = child_node_list[1]
+                }
+                if (now_node.getId() == 4021) {
+                    now_node = temp_node
+                }
+                else{
+                    now_node = child_node_list[pick_index]
+                }
             }
-            else {
+            else
                 now_node = child_node_list[0]
-                pick_string.add(child_node_list[0].getName())
-            }
         }
+
         else
             break
         println(" ")
         println("#--------------------------------#")
     }
-    for (arg in pick_string)
+    for (arg in aac_tree.getString())
     {
         print(arg)
     }
